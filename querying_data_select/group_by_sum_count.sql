8b4564ae160292c6602ebef4c6de0cec7a8027e8
ANKITA MATHUR <ankita.mathur227@gmail.com>
2025-12-12 22:19:36 -0600

    group_by_sum_count.sql

---
+++ querying_data_select/group_by_sum_count.sql
@@ -0,0 +1,15 @@
+-- Finding the total score for each country
+SELECT 
+    country,
+    SUM(score) AS total_score
+FROM customers
+GROUP BY country
+
+-- Finding the total score and total number of customers for each country
+SELECT 
+    country,
+    SUM(score) AS total_score,
+    COUNT(id) AS total_customers
+FROM customers
+GROUP BY country
+
